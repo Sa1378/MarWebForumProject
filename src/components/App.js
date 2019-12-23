b3c966503ba935f3e8cd041f8563d8bc37a6011a
Rez <reza.amini.majd1379@gmail.com>
2019-12-23 19:23:42 +0330

    profile complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route} from "react-router-dom";
+import React, {Component} from "react";
+import {BrowserRouter, Route} from "react-router-dom";
 import "../static/css/App.css";
 import Header from "./Header";
 import MainPage from "./MainPage";
@@ -13,27 +13,32 @@ import CreateChannel from "./CreateChannel";
 import Notifications from "./Notifications";
 import Search from "./Search";
 
-class App extends Component{
-  render (){
-      return (<div style={{whiteSpace:"pre-wrap"}}>
-        <Header/>
-        <BrowserRouter>
-          <div className="mainContainer">
-            <Route exact path="/" component={MainPage} />
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register} />
-            <Route path="/profile/:username" component={Profile} />
-            <Route path="/editprofile" component={EditProfile} />
-            <Route path="/newpost" component={NewPost} />
-            <Route path="/channel/:channelName" component={Channel} />
-            <Route path="/createchannel" component={CreateChannel} />
-            <Route path="/notifications" component={Notifications} />
-            <Route path="/search/:searchQuery" component={Search} />
-          </div>
-        </BrowserRouter>
-      </div>
-      );
-  };
+class App extends Component {
+
+    state = {
+      my_name : 'reza'
+    };
+
+    render() {
+        return (<div style={{whiteSpace: "pre-wrap"}}>
+                <Header/>
+                <BrowserRouter>
+                    <div className="mainContainer">
+                        <Route exact path="/" component={MainPage}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/register" component={Register}/>
+                        <Route path="/profile/:username" my_name ={this.state.my_name} component={Profile}/>
+                        <Route path="/editprofile" component={EditProfile}/>
+                        <Route path="/newpost" component={NewPost}/>
+                        <Route path="/channel/:channelName" component={Channel}/>
+                        <Route path="/createchannel" component={CreateChannel}/>
+                        <Route path="/notifications" component={Notifications}/>
+                        <Route path="/search/:searchQuery" component={Search}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    };
 }
 
 export default App;
