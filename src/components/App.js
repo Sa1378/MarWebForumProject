08ab6a1c62f5db156622c7afeb37021ed5072a71
alireza <alirezashateri7@gmail.com>
2019-12-23 19:43:24 +0330

    Post Component added with its initial Style

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route} from "react-router-dom";
+import React, {Component} from "react";
+import {BrowserRouter, Route} from "react-router-dom";
 import "../static/css/App.css";
 import Header from "./Header";
 import MainPage from "./MainPage";
@@ -12,28 +12,30 @@ import Channel from "./Channel";
 import CreateChannel from "./CreateChannel";
 import Notifications from "./Notifications";
 import Search from "./Search";
+import Post from "./post/Post";
 
-class App extends Component{
-  render (){
-      return (<div style={{whiteSpace:"pre-wrap"}}>
-        <Header/>
-        <BrowserRouter>
-          <div className="mainContainer">
-            <Route exact path="/" component={MainPage} />
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register} />
-            <Route path="/profile/:username" component={Profile} />
-            <Route path="/editprofile" component={EditProfile} />
-            <Route path="/newpost" component={NewPost} />
-            <Route path="/channel/:channelName" component={Channel} />
-            <Route path="/createchannel" component={CreateChannel} />
-            <Route path="/notifications" component={Notifications} />
-            <Route path="/search/:searchQuery" component={Search} />
-          </div>
-        </BrowserRouter>
-      </div>
-      );
-  };
+class App extends Component {
+    render() {
+        return (<div style={{whiteSpace: "pre-wrap"}}>
+                <Header/>
+                <BrowserRouter>
+                    <div className="mainContainer">
+                        <Route exact path="/" component={MainPage}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/register" component={Register}/>
+                        <Route path="/profile/:username" component={Profile}/>
+                        <Route path="/editprofile" component={EditProfile}/>
+                        <Route path="/newpost" component={NewPost}/>
+                        <Route path="/channel/:channelName" component={Channel}/>
+                        <Route path="/createchannel" component={CreateChannel}/>
+                        <Route path="/notifications" component={Notifications}/>
+                        <Route path="/search/:searchQuery" component={Search}/>
+                        <Route path={"/post/:name"} component={Post}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    };
 }
 
 export default App;
