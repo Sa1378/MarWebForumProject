a477fb76b8ed1b7d7225cd83ddd4e776807e0243
alireza <alirezashateri7@gmail.com>
2019-12-23 19:52:13 +0330

    first commit

--- src/components/user profile/Profile.js
+++ src/components/user profile/Profile.js
@@ -1,63 +1,18 @@
-import React, {Component} from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import SettingsIcon from '@material-ui/icons/Settings';
-import ImageAvatars from "./Avatar";
-import SimpleTabs from "./postOrChannel";
-import Data from "./profileData";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Follow from "./follow";
+import React, { Component } from "react";
 
-
-class Profile extends Component {
-    state = {
-        my_name: 'reza',
-        username: null,
-        avatar_src: null,
-        numberOfPosts: 20,
-        numberOfFollower: 354,
-        numberOfFollowing: 323,
-        followed: false,
-    };
-
-    componentDidMount() {
-        this.setState(() => this.props.match.params);
+class Profile extends Component{
+    state={
+        username:null,
     }
-
-    render() {
-        return (
-            <React.Fragment>
-                <CssBaseline/>
-                <Container maxWidth="sm">
-                    <Typography component="div" style={{backgroundColor: 'white', height: '88vh',}}
-                                className="border rounded">
-                        <div className="d-flex justify-content-end p-2">
-                            {this.formatSetting()}
-                        </div>
-                        {this.formatAvatar()}
-                        <Data name={this.state.username} numberOfPosts={this.state.numberOfPosts}
-                              numberOfFollower={this.state.numberOfFollower}
-                              numberOfFollowing={this.state.numberOfFollowing}/>
-                        <Follow followed={this.state.followed} my_name={this.state.my_name}
-                                username={this.state.username}/>
-                        <SimpleTabs/>
-                    </Typography>
-                </Container>
-            </React.Fragment>
+    componentDidMount () {
+        this.setState(()=>this.props.match.params); // age dasti this.state ro meghdar bdim nmishe
+    }
+    render (){
+        return (<div>
+            This is {this.state.username}'s Profile Page!
+        </div>
         );
     };
-
-    formatAvatar() {
-        return <ImageAvatars avatarSrc={this.state.avatar_src}/>
-    }
-
-    formatSetting() {
-        if (this.state.username !== this.state.my_name)
-            return;
-        else return <SettingsIcon fontSize="large"/>
-    }
 }
-
 
 export default Profile;
