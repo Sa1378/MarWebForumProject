0a391f17630912fa5e649e1764eecbd30d1c9588
Rez <reza.amini.majd1379@gmail.com>
2020-01-01 16:31:47 +0330

    refactor Code

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, {Component} from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import SimpleTabs from "../TwoTab";
 
-function Login(){
-    return (
-        <React.Fragment>
-            <CssBaseline/>
-            <Container maxWidth="sm">
-                <Typography component="div" style={{backgroundColor: 'white', height: '58vh',}}
-                            className="border rounded mt-5">
-                    <SimpleTabs name1="Sign in" name2="Sing up" page="login"/>
-                </Typography>
-            </Container>
-        </React.Fragment>
-    );
+class Login extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <CssBaseline/>
+                <Container maxWidth="sm">
+                    <Typography component="div" style={{backgroundColor: 'white', height: '58vh',}}
+                                className="border rounded mt-5">
+                        <SimpleTabs name1="Sign in" name2="Sing up" page="login"/>
+                    </Typography>
+                </Container>
+            </React.Fragment>
+        );
+    }
 
 }
 
